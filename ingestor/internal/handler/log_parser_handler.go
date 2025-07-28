2f22312dfa2b31d42129373dd774510379efa6d8
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-07-29 05:11:28 +0530

    added grpc handler

--- ingestor/internal/handler/log_parser_handler.go
+++ ingestor/internal/handler/log_parser_handler.go
@@ -2,26 +2,36 @@ package handler
 
 import (
 	"context"
-	"log"
+	"fmt"
 
+	"github.com/ShivangSrivastava/sentinel/ingestor/internal/service"
 	pb "github.com/ShivangSrivastava/sentinel/proto/logging"
 )
 
 type LogIngestorServer struct {
 	pb.UnimplementedLogIngestorServer
+	service service.LogParserService
 }
 
-func NewLogIngestorServer() *LogIngestorServer {
-	return &LogIngestorServer{}
+func NewLogIngestorServer(s service.LogParserService) *LogIngestorServer {
+	return &LogIngestorServer{
+		service: s,
+	}
 }
 
 func (s *LogIngestorServer) SendLogParser(
 	ctx context.Context, req *pb.LogRequest,
 ) (*pb.LogResponse, error) {
-	log.Printf(req.GetJsonPayload())
+	err := s.service.CreateLogParser(req.GetJsonPayload())
+	if err != nil {
+		return &pb.LogResponse{
+			Success: false,
+			Message: fmt.Sprintf("%v", err),
+		}, err
+	}
 	return &pb.LogResponse{
 		Success: true,
-		Message: "",
+		Message: "Successfully added",
 	}, nil
 
 }
--- ingestor/main.go
+++ ingestor/main.go
@@ -9,6 +9,8 @@ import (
 
 	"github.com/ShivangSrivastava/sentinel/ingestor/internal/database"
 	"github.com/ShivangSrivastava/sentinel/ingestor/internal/handler"
+	"github.com/ShivangSrivastava/sentinel/ingestor/internal/repo"
+	"github.com/ShivangSrivastava/sentinel/ingestor/internal/service"
 	pb "github.com/ShivangSrivastava/sentinel/proto/logging"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
@@ -30,8 +32,13 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	defer listener.Close()
+	logParserRepo := repo.NewLogParserRepo(pgDB)
+	logParserservice := service.NewLogParserService(logParserRepo)
+	logParserHandler := handler.NewLogIngestorServer(logParserservice)
+
 	grpcServer := grpc.NewServer()
-	pb.RegisterLogIngestorServer(grpcServer, handler.NewLogIngestorServer())
+
+	pb.RegisterLogIngestorServer(grpcServer, logParserHandler)
 	log.Println("LogIngestor gRPC server running on :50051")
 	if err := grpcServer.Serve(listener); err != nil {
 		log.Fatalf("Failed to serve: %v", err)

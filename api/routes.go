cb82bf1af13fa891855acb8f8a81808e2e436429
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-08-06 10:38:57 +0530

    added logging middleware

--- api/routes.go
+++ api/routes.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/ShivangSrivastava/sentinel/internal/logformat"
+	"github.com/ShivangSrivastava/sentinel/pkg/middleware"
 )
 
 func NewRouter(logformatHandler logformat.LogFormatHandler) http.Handler {
@@ -13,6 +14,9 @@ func NewRouter(logformatHandler logformat.LogFormatHandler) http.Handler {
 	registerLogformatRoutes(mux, logformatHandler)
 
 	return http.StripPrefix("/v1", mux)
+	handler := middleware.LoggingMiddleware(mux)
+
+	return http.StripPrefix("/v1", handler)
 }
 
 func registerLogformatRoutes(mux *http.ServeMux, h logformat.LogFormatHandler) {
